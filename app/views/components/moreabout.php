83d95cce4f76ab58aa53fab9428a5d0439325212
Adam Moore <adamaoc@gmail.com>
2015-06-11 07:10:44 -0500

    updated image with gravitar image

--- app/views/components/moreabout.php
+++ app/views/components/moreabout.php
@@ -1,6 +1,6 @@
 <div class="more-about">
 	<div class="img-wrap">
-		<img src="http://distilleryimage2.ak.instagram.com/7ae4dea876ea11e39c8f12a4e8421c6a_8.jpg" alt="Learn more about Adam and ampnet media" />
+		<img src="https://en.gravatar.com/userimage/18223801/b646e223db93275a5f96ec250c941ca7.jpg?size=300" alt="Learn more about Adam and ampnet media" />
 	</div>
 	<div class="text-wrap">
 		<p><?= $data ?></p>
