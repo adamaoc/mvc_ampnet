09ace7de37971280d9ebecf310dd51141c5f39b2
Adam Moore <adamaoc@gmail.com>
2017-09-27 21:55:37 -0500

    Update core_strengths.php

--- app/views/components/core_strengths.php
+++ app/views/components/core_strengths.php
@@ -1,6 +1,7 @@
 <h2>Core strengths include:</h2>
 <ul class="strength-list">
-	<li>Front-end (HTML | CSS | JS | RWD)</li>
-	<li>Back-end (PHP | MySQL)</li>
-	<li>Platforms (WordPress)</li>
+	<li>Web Development</li>
+	<li>Web Video and Photography</li>
+	<li>Web Advertising</li>
+	<li>Social Media</li>
 </ul>
